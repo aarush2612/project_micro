e7ec721c54ec07480da59a44ecd45f9ce274be8a
aarush2612 <142479283+aarush2612@users.noreply.github.com>
2025-04-30 18:23:53 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,72 +1,55 @@
 #include <at89c5131.h>
 #include "lcd.h"																				//Driver for interfacing lcd 
-#include "mcp3008.h"																		//Driver for interfacing ADC ic MCP3008
+#include "mcp3008.h"		//Driver for interfacing ADC ic MCP3008
+#include <math.h>
 
 
-char adc_ip_data_ascii[6]={0,0,0,0,0,'\0'};							//string array for saving ascii of input sample
-char adc_ip_data_ascii1[6]={0,0,0,0,0,'\0'};	
-code unsigned char display_msg1[]="Volt.: ";						//Display msg on 1st line of lcd
-code unsigned char display_msg2[]=" mV";//Display msg on 2nd line of lcd
-unsigned int adc_data_prev = 0;
-unsigned int count = 0;
-unsigned int arr[10] = {0,0,0,0,0,0,0,0,0,0};
-unsigned int i = 0;
-unsigned int flag = 0;
+char adc_ip_data_ascii[6]={0,0,0,0,0,'\0'};							//string array for saving ascii of input sample	
 
 void main(void)
 {
-	unsigned int adc_data_curr=0,temp_data=0,adc_data=0,highest=0;
-	unsigned int x=0,y=0;
+	unsigned int i=0,counter = 0;
+	int s = 0, c = 0;
+	unsigned int x=0;
 	
 	spi_init();																					
 	adc_init();
   lcd_init();
-	msdelay(100);
+	msdelay(1000);
 	
-	while(1){
+	while(counter<400){
 	  x = adc(4);
-	  adc_data_curr = (int) (x*4.8875); 		//Converting received 10 bits value to milli volt (3.3*1000*i/p /1023)
-		if(adc_data_curr > adc_data_prev){
-			adc_data = adc_data_curr - adc_data_prev;
+	  switch(counter%4) {
+			case 0:
+				s+=x;
+			  counter++;
+			  break;
+			
+			case 1:
+				c+=x;
+			  counter++;
+			  break;
+			
+			case 2:
+				s-=x;
+			  counter++;
+			  break;
+			
+			case 3:
+				counter++;
+				c-=x;
+			  break;
 		}
-		else if(adc_data_prev > adc_data_prev){
-			adc_data = adc_data_prev - adc_data_curr;
-		}
-		
-	  int_to_string(adc_data, adc_ip_data_ascii);
-		lcd_cmd(0x80);
-		lcd_write_string("Volt: ");
-	  lcd_write_string(adc_ip_data_ascii);
-		adc_data_prev = adc_data_curr;
-		if(highest<adc_data){
-			  if(count<4){
-					count++;
-				}
-			  if(adc_data-highest<=0.1*adc_data && count==3 && i<10){
-					arr[i++] = adc_data;
-				}
-				highest = adc_data;
-			  count+=1;
-		}
-		else if(highest>adc_data){
-			if(highest - adc_data<=0.1*highest && i<10){
-				arr[i++] = adc_data;
-			}
-
-		}
-		
-		lcd_cmd(0xc0);
-		int_to_string(highest, adc_ip_data_ascii);
-		lcd_write_string("Amplitude: ");
-		lcd_write_string(adc_ip_data_ascii);
-		if(i == 10){
-			count = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] + arr[10])/10;
-			lcd_cmd(0x80);
-			int_to_string(count, adc_ip_data_ascii);
-			lcd_write_string("Final: ");
-			lcd_write_string(adc_ip_data_ascii);
-			while(1);
-		}
-	  fiveusdelay(10);
+		//459.5us from x = adc(4) to c-=x;
+		sixtyusdelay(159);
 	}
+	c = (int) (c*4.8875)/200;// 4.8875 is because of 5/1023
+	s = (int) (s*4.8875)/200;
+	//lcd_cmd(0x80);
+	
+	s = s*s+c*c;
+	int_to_string(s, adc_ip_data_ascii);
+	lcd_write_string("Amplitude: ");
+	lcd_write_string(adc_ip_data_ascii);
+	while(1);
 }
